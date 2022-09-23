9397b2d7d406242927f54d581bf68e0ff91e2af2
alex <albabenka@gmail.com>
2022-09-23 21:18:47 +0300

    dialogs

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import dialogs from './Dialogs.module.css'
+
+const Dialogs = (props) => {
+
+    return (
+        <div>
+            Diologs
+        </div>
+    )
+}
+
+export default Dialogs;
